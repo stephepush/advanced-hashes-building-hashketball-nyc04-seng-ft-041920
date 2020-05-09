e3490a85df4a104f08846faf85f2fb9ad534fcd7
Stephen Peters <7speter@gmail.com>
2020-05-09 23:35:54 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -102,13 +102,13 @@ def game_hash
       {
         :player_name => "Ben Gordon",
         :number => 8,
-        :shoe => 14,
-        :points => 24,
-        :rebounds => 12,
-        :assists => 12,
-        :steals => 4,
-        :blocks => 5,
-        :slam_dunks => 5
+        :shoe => 15,
+        :points => 33,
+        :rebounds => 3,
+        :assists => 2,
+        :steals => 1,
+        :blocks => 1,
+        :slam_dunks => 0
       },
       {
         :player_name => "Kemba Walker",
