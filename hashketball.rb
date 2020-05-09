9e5f81b2fdaaa151ac1acb543bf2529a6a2a3783
Stephen Peters <7speter@gmail.com>
2020-05-09 23:31:15 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -2,7 +2,7 @@
 game_hash = {
   :home => {
     :team_name => "Brooklyn Nets",
-    :colors => ["Black", "White"]
+    :colors => ["Black", "White"],
     :players => [
       {
         :player_name => "Alan Anderson",
